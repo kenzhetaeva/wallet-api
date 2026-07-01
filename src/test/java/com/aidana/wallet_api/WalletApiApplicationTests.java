1186fbed42723febd8ef7dd2453c6b9c6fb1fe23
Aidana Kenzhetaeva <kenzhetaeva11@gmail.com>
2026-07-01 11:45:37 +0600

    Fix tests

--- src/test/java/com/aidana/wallet_api/WalletApiApplicationTests.java
+++ src/test/java/com/aidana/wallet_api/WalletApiApplicationTests.java
@@ -1,10 +1,11 @@
 package com.aidana.wallet_api;
 
+import com.aidana.wallet_api.config.IntegrationTestBase;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WalletApiApplicationTests {
+class WalletApiApplicationTests extends IntegrationTestBase {
 
 	@Test
 	void contextLoads() {
